6576f7f68b43c281dbabfdb89796966672ba7726
andriiskrypnyk <andre.beltukov5656@gmail.com>
2024-01-10 11:40:23 +0200

    Update user tests and views docs

--- apps_generic/whodidit/test.py
+++ apps_generic/whodidit/test.py
@@ -1,30 +1,37 @@
-from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
 import pytest
 
 
-@dataclass
-class UserData:
+class AbstractUserAuth(ABC):
+    @abstractmethod
+    def default_user(self, *args, **kwargs):
+        """Base implementation for creating a default user."""
+
+    @abstractmethod
+    def authenticate_user(self, *args, **kwargs):
+        """Base implementation for authenticating a user."""
+
+
+class BaseUserAuth(AbstractUserAuth):
     username = 'Test User name'
     first_name = 'Test First NAME'
     last_name = 'Test Last Name'
     password = 'QW12W23333jjg'
 
+    @pytest.fixture
+    def default_user(self, django_user_model):
+        user = django_user_model.objects.create_user(
+            username=self.username,
+            first_name=self.first_name,
+            last_name=self.last_name,
+            password=self.password
+        )
+        user.set_password('pass5678')
+        user.save()
+        return user
 
-@pytest.fixture
-def default_user(django_user_model, user_data=UserData):
-    user = django_user_model.objects.create_user(
-        username=user_data.username,
-        first_name=user_data.first_name,
-        last_name=user_data.last_name,
-        password=user_data.password
-    )
-    user.set_password('pass5678')
-    user.save()
-    return user
-
-
-@pytest.fixture
-def authenticated_user(client, default_user):
-    client.force_login(default_user)
-    return default_user
+    @pytest.fixture
+    def authenticate_user(self, client, default_user):
+        client.force_login(default_user)
+        return default_user
