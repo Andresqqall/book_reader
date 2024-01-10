ff2d1b4e1bb790c82a45018854ef16ad6419aa29
andriiskrypnyk <andre.beltukov5656@gmail.com>
2024-01-10 11:40:53 +0200

    Add celery beat task

--- book_reader/celery.py
+++ book_reader/celery.py
@@ -1,3 +1,4 @@
+from celery.schedules import crontab
 from django.conf import settings
 from celery import Celery
 import os
@@ -6,3 +7,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'book_reader.settings')
 app = Celery('book_reader')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    app.conf.timezone = 'UTC'
+    app.conf.beat_schedule = {
+        'make-analytics-every-day': {
+            'task': 'apps.analytics.tasks.collect_analytics',
+            # run at 1:01 AM PT (9:01 AM UTC)
+            'schedule': crontab(hour=7, minute=56),
+        },
+    }
