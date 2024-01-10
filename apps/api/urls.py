651ea773dd4a4cb6f0b377d993e74fa10262f7b0
andriiskrypnyk <andre.beltukov5656@gmail.com>
2024-01-10 11:38:07 +0200

    Update the API app by adding new URLs

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('auth/', include('apps.auth.urls')),
-    path('users/', include('apps.users.urls'))
-
+    path('users/', include('apps.users.urls')),
+    path('books/', include('apps.books.urls')),
+    path('library/', include('apps.library.urls')),
+    path('analytics/', include('apps.analytics.urls')),
 ]
